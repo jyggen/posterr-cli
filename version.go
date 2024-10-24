513052def755df60f06bef804b91c07208854aca
Jonas Stendahl <jonas@stendahl.me>
2024-10-24 19:08:18 +0200

    fix: lint issue

--- version.go
+++ version.go
@@ -11,7 +11,7 @@ type VersionFlag string
 func (v VersionFlag) Decode(_ *kong.DecodeContext) error { return nil }
 func (v VersionFlag) IsBool() bool                       { return true }
 func (v VersionFlag) BeforeApply(app *kong.Kong, _ kong.Vars) error {
-	app.Printf(version)
+	app.Printf("%s", version)
 	app.Exit(0)
 	return nil
 }
